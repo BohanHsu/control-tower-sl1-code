a522a47324df90567512883afb169975a5c9a29b
BohanHsu <ilovebarce@gmail.com>
2020-02-18 23:57:36 -0500

    try

--- server/src/server.js
+++ server/src/server.js
@@ -21,9 +21,13 @@ if (process.env.NODE_ENV === 'production') {
 
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/control-tower');
 
+MONGOLAB_URI
+
 const MONGODB_URL =
   process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL || 'mongodb://localhost/control-tower';
+
+console.log(MONGODB_URL);
 
 mongoose.connect(MONGODB_URL);
 
