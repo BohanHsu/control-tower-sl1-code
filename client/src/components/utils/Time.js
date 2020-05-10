1a487a95577bbba9a98b8be70fe651f57d41e646
BohanHsu <ilovebarce@gmail.com>
2020-05-10 17:26:39 -0400

    ui

--- client/src/components/utils/Time.js
+++ client/src/components/utils/Time.js
@@ -1,12 +1,26 @@
 import React, {useEffect, useState, useCallback} from 'react';
+const moment = require('moment-timezone');
 
 function Time(props) {
+  let localTime = null;
+  let nyTime = null;
 
-  return (
-    <p>
-      <span>{props.time != null && new Date(props.time).toLocaleString()}</span>
-    </p>
-  );
+  if (props.time != null) {
+    const dateOfTime = new Date(props.time);
+    localTime = dateOfTime.toLocaleString()
+    nyTime = moment(props.time).tz("America/New_York").format('MM/DD/YYYY HH:mm:ss');
+  }
+
+
+
+  return props.time 
+    ? (
+      <p>
+        <span>{`NYT: ${nyTime},`}</span>
+        <span style={{'color': '#A9A9A9'}}>{` (LOCAL: ${localTime})`}</span>
+        </p>
+    )
+    : (<p></p>);
 }
 
 export default Time;
